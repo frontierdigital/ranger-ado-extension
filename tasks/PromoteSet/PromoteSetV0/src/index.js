389be158ecbc0b1846e35264daea8c1c2d8e807f
Fraser Davidson <fraser@frontierdigital.net>
2023-08-04 15:50:09 +0100

    WIP

--- tasks/PromoteSet/PromoteSetV0/src/index.js
+++ tasks/PromoteSet/PromoteSetV0/src/index.js
@@ -4,7 +4,10 @@ const tl = require('azure-pipelines-task-lib/task');
 
 async function run() {
   try {
+    const gitUserEmail = tl.getInput('gitUserEmail', true);
+    const gitUserName = tl.getInput('gitUserName', true);
     const organisationName = tl.getInput('organisationName', true);
+    const pat = tl.getInput('pat', true);
     const projectName = tl.getInput('projectName', true);
     const targetEnvironment = tl.getInput('targetEnvironment', true);
 
@@ -20,6 +23,15 @@ async function run() {
         '--target-environment',
         targetEnvironment,
       ],
+      {
+        env: {
+          ...process.env,
+          RANGER_ADO_PAT: pat,
+          RANGER_GIT_USEREMAIL: gitUserName,
+          RANGER_GIT_USERNAME: gitUserEmail,
+        },
+        silent: false,
+      },
     );
 
     tl.setResult(tl.TaskResult.Succeeded, 'Success');
